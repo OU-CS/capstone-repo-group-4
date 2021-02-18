30216249de1b7215f867afc856b6856052e824a8
s-fletcher <sfletchertx@gmail.com>
2021-02-17 22:24:33 -0600

    Change folder structure

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,7 +1,9 @@
 import { NextPage } from 'next';
+import Nav from '../components/nav';
 
 const Index: NextPage = () => (
     <div>
+        <Nav />
         <h1>Hello World!</h1>
     </div>
 );
---
+++ frontend/src/templates/gallery/index.tsx
@@ -0,0 +1,11 @@
+import { FC } from 'react';
+import Card from './components/card';
+
+const Gallery: FC = () => (
+    <div>
+        <h1>Gallery</h1>
+        <Card />
+    </div>
+);
+
+export default Gallery;
