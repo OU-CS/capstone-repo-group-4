09bdc833e64eff192f1d33c010fe01c4068b7076
Connor Sharp <cjsharp99@gmail.com>
2021-02-18 16:55:15 -0600

    restructured the aws backend with a new template and added deploy script

---
+++ roam-backend/src/handlers/find-user-token.js
@@ -0,0 +1,27 @@
+/**
+ * A simple example includes a HTTP get method to get return a message
+ */
+exports.findUserToken = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getUserTokens only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    var responseStatusCode = 400; 
+    var responseBody = "token not found";
+    if(event.userToken == 'TEST') {
+        responseStatusCode = 200;
+        responseBody = "token found"
+    }
+
+
+    const response = {
+        statusCode: responseStatusCode,
+        body: responseBody
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
