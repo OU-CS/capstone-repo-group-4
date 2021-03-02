e3357259d35204ee10171867c4168099bd1982a8
Connor Sharp <cjsharp99@gmail.com>
2021-03-02 17:52:58 -0600

    added a lambda function and test that returns all the properties in the Database

---
+++ backend/src/handlers/get-all-properties.js
@@ -0,0 +1,28 @@
+// imports function that connects to the database
+const databaseConnector = require('../helpers/db-connector');
+
+/**
+ * A simple example of a lambda that returns data from the Database
+ */
+exports.getAllProperties = async (event) => {
+    if (event.httpMethod !== 'GET') {
+        throw new Error(`getUserTokens only accept GET method, you tried: ${event.httpMethod}`);
+    }
+    
+    // All log statements are written to CloudWatch
+    console.info('received:', event);
+
+    // Generates a SQL statement for returning all the properties from the database
+    // return data from the SQL statement are saved in sqlResponse
+    let sqlStatement = 'SELECT * FROM property';
+    let sqlResponse = await databaseConnector.databaseQuery(sqlStatement);
+
+    const response = {
+        statusCode: 200,
+        body: sqlResponse
+    };
+
+    // All log statements are written to CloudWatch
+    console.info(`response from: ${event.path} statusCode: ${response.statusCode} body: ${response.body}`);
+    return response;
+}
