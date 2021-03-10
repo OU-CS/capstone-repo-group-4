1566e01ccc7e55d1bc68e96893747b9f74f4c5b0
s-fletcher <sfletchertx@gmail.com>
2021-03-09 21:53:47 -0600

    Catch error on lambda fail

--- backend/src/handlers/get-all-properties.ts
+++ backend/src/handlers/get-all-properties.ts
@@ -1,6 +1,6 @@
 import { APIGatewayProxyHandler } from 'aws-lambda';
 import { databaseQuery } from '../helpers/db-connector';
-import { successResponse } from '../helpers/responses';
+import { failedResponse, successResponse } from '../helpers/responses';
 
 /**
  * A simple example of a lambda that returns data from the Database
@@ -8,10 +8,14 @@ import { successResponse } from '../helpers/responses';
 export const getAllProperties: APIGatewayProxyHandler = async (event) => {    
     console.info('received:', event);
 
-    // Generates a SQL statement for returning all the properties from the database
-    // return data from the SQL statement are saved in sqlResponse
-    const sqlStatement = 'SELECT * FROM property';
-    const sqlResponse = await databaseQuery(sqlStatement);
+    try {
+        // Generates a SQL statement for returning all the properties from the database
+        // return data from the SQL statement are saved in sqlResponse
+        const sqlStatement = 'SELECT * FROM property';
+        const sqlResponse = await databaseQuery(sqlStatement);
 
-    return successResponse(sqlResponse);
+        return successResponse(sqlResponse);
+    } catch (e) {
+        return failedResponse(500, e.message);
+    }
 }
