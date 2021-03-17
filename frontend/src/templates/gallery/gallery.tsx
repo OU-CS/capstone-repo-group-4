7f293c669bd03e2f10248407e15792b11f62306f
s-fletcher <sfletchertx@gmail.com>
2021-03-16 20:08:42 -0500

    Add date picker

--- frontend/src/templates/gallery/gallery.tsx
+++ frontend/src/templates/gallery/gallery.tsx
@@ -2,9 +2,11 @@ import { SimpleGrid } from '@chakra-ui/react';
 import { FC } from 'react';
 import { Layout } from '../../components';
 import { Card } from './components/card';
+import { DateRangePicker } from './components/date-picker';
 
 const Gallery: FC = () => (
     <Layout>
+        <DateRangePicker />
         <SimpleGrid minChildWidth="300px" width="100%" spacing="25px">
             <Card />
             <Card />
