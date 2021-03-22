6662da74ece0a39325ff31570b14bdbf42526f4a
s-fletcher <sfletchertx@gmail.com>
2021-03-21 23:02:19 -0500

    Remove unused files

---
+++ backend/__tests__/unit/handlers/get-property-by-id.test.ts
@@ -0,0 +1,25 @@
+/**
+ * TODO: This test file needs to be fixed
+ * We'll probably want to test the functions within the getAllItemsHandler() as
+ * opposed to the function itself due to AWS Handlers having complex parameters.
+ */
+
+// Import all functions from get-all-items.js
+// import { getPropertyByID } from '../../../src/handlers/get-property-by-id.ts';
+
+// This includes all tests for getAllItemsHandler()
+// describe('Test getSingleProperty', () => {
+
+//     it('should return a single property', async () => {
+//         const event = {
+//             httpMethod: 'GET',
+//             userToken: 'TEST',
+//             id: '291e3b22-2dd9-4d5b-a5b8-94dee4f34402'
+//         }
+
+//         // Invoke helloFromLambdaHandler()
+//         const result = await lambda.getPropertyByID(event);
+
+//         console.log(result);
+//     });
+// });
