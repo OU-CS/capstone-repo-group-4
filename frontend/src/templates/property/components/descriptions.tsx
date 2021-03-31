c4f3c838f79f1ee80bef16f6385b2142ed6482a4
s-fletcher <sfletchertx@gmail.com>
2021-03-30 23:20:01 -0500

    Add location component

--- frontend/src/templates/property/components/descriptions.tsx
+++ frontend/src/templates/property/components/descriptions.tsx
@@ -1,5 +1,6 @@
 import { Heading, Text, VStack } from '@chakra-ui/layout';
 import { FC } from 'react';
+import { Location } from './location';
 
 export const Description: FC = ({ children }) => (
     <VStack align="flex-start">
@@ -10,5 +11,10 @@ export const Description: FC = ({ children }) => (
             .map((paragraph, i) => (
                 <Text key={`paragraph-${i}`}>{paragraph}</Text>
             ))}
+
+        <Heading pt="12px" size="md">
+            Location
+        </Heading>
+        <Location address="University of Oklahoma, Norman OK" />
     </VStack>
 );
