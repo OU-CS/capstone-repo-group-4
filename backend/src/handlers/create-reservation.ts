b591eb4ec5313f0bbac07078d71df240a9139462
Connor Sharp <cjsharp99@gmail.com>
2021-04-01 15:48:56 -0500

    added base for create reservation lambda

---
+++ backend/src/handlers/create-reservation.ts
@@ -0,0 +1,74 @@
+import { APIGatewayProxyEventQueryStringParameters, APIGatewayProxyHandler } from 'aws-lambda';
+import { getAllPropertiesQuery } from '../helpers/queries/get-all-properties-query';
+import { failedResponse, successResponse } from '../helpers/responses';
+
+export type ParamProps = APIGatewayProxyEventQueryStringParameters | null;
+
+export type CreateReservationPropertyParams = {
+    startTime: string;
+    endTime: string;
+    price: number;
+    propertyID: string;
+    guestID: string;
+    hostID: string;
+};
+
+/**
+ * Validates all query string parameters from api event
+ */
+export const validateParameters = (params: ParamProps): CreateReservationPropertyParams => {
+    if (!params) {
+        throw new Error('No query parameters were specified');
+    }
+
+    const { startTime, endTime, price, propertyID, guestID, hostID } = params;
+
+    if (!startTime) {
+        throw new Error('No startTime was specified');
+    }
+    if (!endTime) {
+        throw new Error('No endTime was specified');
+    }
+    if (!price) {
+        throw new Error('No price was specified');
+    }
+    if (!propertyID) {
+        throw new Error('No propertyID was specified');
+    }
+    if (!guestID) {
+        throw new Error('No guestID was specified');
+    }
+    if (!hostID) {
+        throw new Error('No hostID was specified');
+    }
+    return { startTime, endTime, price: Number(price), propertyID, guestID, hostID };
+};
+
+/**
+ * A simple example of a lambda that returns data from the Database
+ */
+export const getAllProperties: APIGatewayProxyHandler = async (event) => {
+    console.info('received:', event);
+    let startTime: string;
+    let endTime: string;
+
+    // Validate query parameters
+    try {
+        ({ startTime, endTime } = validateParameters(event.queryStringParameters));
+    } catch (e) {
+        console.error(e);
+        return failedResponse(400, e);
+    }
+
+    try {
+        // Generates a SQL statement for returning all the properties from the database
+        // return data from the SQL statement are saved in sqlResponse
+        const sqlResponse = await getAllPropertiesQuery(startTime, endTime);
+
+        console.info('success:', sqlResponse);
+        return successResponse(sqlResponse);
+    } catch (e) {
+        console.error(e);
+        return failedResponse(500, e);
+    }
+};
