04f288f3ef61eb105f3a37745044572ecf6e36ea
s-fletcher <sfletchertx@gmail.com>
2021-04-15 15:32:12 -0500

    Remove old modal

--- frontend/src/components/index.ts
+++ frontend/src/components/index.ts
@@ -2,5 +2,4 @@ export { Button } from './inputs/button';
 export { Input, PasswordInput } from './inputs/input';
 export { Layout } from './layout/layout';
 export { Logo } from './logo/logo';
-export { Modal } from './modal/modal';
 export { Nav } from './nav/nav';
