296446562e56f4005cd6d413ccd143acc3aa1cdd
Connor Sharp <cjsharp99@gmail.com>
2021-04-27 16:10:34 -0500

    added a new query in place of the old one that is more accurate towards achieving our response results

--- backend/src/helpers/queries/get-all-properties-query.ts
+++ backend/src/helpers/queries/get-all-properties-query.ts
@@ -7,8 +7,13 @@ export type Property = {
 
 export const getAllPropertiesQuery = async (startTime: string, endTime: string): Promise<Property[]> =>
     databaseQuery<Property>(`
-        SELECT * FROM property
-            WHERE propertyid IN (SELECT propertyid FROM lease
-            WHERE (startdate NOT BETWEEN '${startTime}' AND '${endTime}')
-            AND (enddate NOT BETWEEN '${startTime}' AND '${endTime}'));
+        SELECT p.propertyid, p.size, p.streetaddr, p.city, p.state, p.zip, p.imgurl, p.pricePerDay, p.name, p.description, p.reservationType, u.userid, u.firstname, u.lastname, u.email, u.phonenumber FROM property as p
+        INNER JOIN host as h
+        ON p.propertyid = h.propertyid
+        INNER JOIN users as u
+        ON u.userid = h.userid
+        WHERE p.propertyid IN ((SELECT propertyid FROM lease
+                    WHERE ('${startTime}' <  startdate AND '${endTime}' < startdate)
+                    OR ('${startTime}' > enddate AND '${endTime}' > enddate)
+                    ) UNION (SELECT propertyid from property WHERE propertyid NOT IN (SELECT propertyid from lease)));
 `);
